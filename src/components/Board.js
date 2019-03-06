617c0ffc6a41b426572a33591a4cd3d387deb578
haslam <haslam414@gmail.com>
2019-03-06 00:26:04 -0500

    test update on Card component

--- src/components/Board.js
+++ src/components/Board.js
@@ -14,7 +14,7 @@ export default class Board extends React.Component {
     this.handlePick = this.handlePick.bind(this);
     this.resetPicks = this.resetPicks.bind(this);
   }
-
+  //Reset the state to init default
   resetPicks() {
     this.setState({
       cards: Array(9).fill(null),
@@ -24,7 +24,7 @@ export default class Board extends React.Component {
   }
   /**
    * pick handler for card play
-   * @param {*} index Takes in zero index of the array item
+   * @param {int} index Takes in zero index of the array item
    */
   handlePick(index) {
     console.log(index);
@@ -32,15 +32,20 @@ export default class Board extends React.Component {
     if(this.state.cards[index] !== null || this.gameWinner(this.state.cards)) 
       return;
 
-    const reCard = this.state.cards.slice(0);
-    reCard[index] = this.state.nextPlay ? 'X' : 'O';
+    const reCard = this.state.cards.slice(0); //create a new copy of the cards array so as not to mutate.
+    reCard[index] = this.state.nextPlay ? 'X' : 'O'; //set index value to X or O depending on nextPlay boolean value
     this.setState({ 
       cards: reCard,
-      nextPlay: !this.state.nextPlay,
+      nextPlay: !this.state.nextPlay, //flip and set nextPlay
      })
     
   }
   
+  /**
+   * Calculate winning pick based on matching set rows
+   * Takes in the complete array
+   * @param {Array} picks returns a single key value of the winning row.
+   */
   gameWinner(picks) {
       const rowMix = [
         [0, 1, 2],
@@ -54,11 +59,15 @@ export default class Board extends React.Component {
       ];
 
       for (let row of rowMix) {
-        const [a, b, c] = row;
+        const [a, b, c] = row;  //destructure array value as a b c
+
+        //check if first value exist and first value is same as second and third [a b c]
+        //if so, it's a match
         if (picks[a] && picks[a] === picks[b] && picks[a] === picks[c]) {
 
-          //set the winning pick
-          this.setState({ win: picks[a] });
+          //set the winning pick [ X or O ]
+          this.setState(() => ({ win: picks[a] }));
+
           //reset cards after 3 seconds to allow user read winning pick before clear
           setTimeout(() => {
             this.resetPicks()
--- src/tests/components/Cards.test.js
+++ src/tests/components/Cards.test.js
@@ -2,11 +2,30 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Cards from '../../components/Cards';
 
+const testCards = Array(9).fill('X');
+let wrapper, handlePick;
+
+beforeEach(() => {
+  handlePick = jest.fn();
+  wrapper = shallow(<Cards cards={testCards} 
+    handlePick={handlePick} />);
+});
+
 describe('Card', () => {
-  const cards = Array(9).fill(null);
-  const handlePick = jest.fn();
-  it('should render as expected', () => {
-    const wrapper = shallow(<Cards cards={cards} handlePick={handlePick} />);
+
+  it('should have 9 boxes', () => {
+    expect(wrapper.children()).toHaveLength(9);
+  });
+
+  it('should ensure event is fired when clicked', () => {
+    wrapper.children().forEach(node => {
+      node.simulate('click');
+      expect(handlePick).toHaveBeenCalled();
+    })
+  });
+
+  it('should ensure component is rendered correctly', () => {
     expect(wrapper).toMatchSnapshot();
   })
+
 })
