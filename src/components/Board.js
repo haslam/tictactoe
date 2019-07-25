648fb20047ddeddd5fa4ea643b70d7ef57ea5366
Hassan <hassan.salami@clearwaterclinical.com>
2019-07-25 13:05:07 -0400

    test files

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,102 +1,99 @@
-import React from 'react';
+import React from 'react'
 import Cards from './Cards'
-import styles from '../styles/board.module.css';
-import { resetButton } from '../styles/button.module.css';
+import styles from '../styles/board.module.css'
+import { resetButton } from '../styles/button.module.css'
 
 export default class Board extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
       cards: Array(9).fill(null),
       nextPlay: true,
       win: ''
     }
-    this.handlePick = this.handlePick.bind(this);
-    this.resetPicks = this.resetPicks.bind(this);
+    this.handlePick = this.handlePick.bind(this)
+    this.resetPicks = this.resetPicks.bind(this)
   }
-  //Reset the state to init default
-  resetPicks() {
+
+  // Reset the state to init default
+  resetPicks () {
     this.setState({
       cards: Array(9).fill(null),
       nextPlay: true,
-      win: '',
+      win: ''
     })
   }
+
   /**
    * pick handler for card play
    * @param {int} index Takes in zero index of the array item
    */
-  handlePick(index) {
-    console.log(index);
-    //head back if card already clicked
-    if(this.state.cards[index] !== null || this.gameWinner(this.state.cards)) 
-      return;
+  handlePick (index) {
+    console.log(index)
+    // head back if card already clicked
+    if (this.state.cards[index] !== null || this.gameWinner(this.state.cards)) { return }
 
-    //create a new copy of the cards array so as not to mutate.
-    const reCard = this.state.cards.slice(0);
+    // create a new copy of the cards array so as not to mutate.
+    const reCard = this.state.cards.slice(0)
 
-    //set index value to X or O depending on nextPlay boolean value
-    reCard[index] = this.state.nextPlay ? 'X' : 'O'; 
+    // set index value to X or O depending on nextPlay boolean value
+    reCard[index] = this.state.nextPlay ? 'X' : 'O'
 
-    this.setState({ 
+    this.setState({
       cards: reCard,
-      nextPlay: !this.state.nextPlay, //flip and set nextPlay
-     })
-    
+      nextPlay: !this.state.nextPlay // flip and set nextPlay
+    })
   }
-  
+
   /**
    * Calculate winning pick based on matching set rows
    * Takes in the complete array
    * @param {Array} picks returns a single key value of the winning row.
    */
-  gameWinner(picks) {
-      const rowMix = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-      ];
+  gameWinner (picks) {
+    const rowMix = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ]
 
-      for (let row of rowMix) {
-        const [a, b, c] = row;  //destructure array value as a b c
+    for (const row of rowMix) {
+      const [a, b, c] = row // destructure array value as a b c
 
-        //check if first value exist and first value is same as second and third [a b c]
-        //if so, it's a match
-        if (picks[a] && picks[a] === picks[b] && picks[a] === picks[c]) {
+      // check if first value exist and first value is same as second and third [a b c]
+      // if so, it's a match
+      if (picks[a] && picks[a] === picks[b] && picks[a] === picks[c]) {
+        // set the winning pick [ X or O ]
+        this.setState(() => ({ win: picks[a] }))
 
-          //set the winning pick [ X or O ]
-          this.setState(() => ({ win: picks[a] }));
+        // reset cards after 3 seconds to allow user read winning pick before clear
+        setTimeout(() => {
+          this.resetPicks()
+        }, 3000)
 
-          //reset cards after 3 seconds to allow user read winning pick before clear
-          setTimeout(() => {
-            this.resetPicks()
-          }, 3000);
-
-          return picks[a];
-        }
+        return picks[a]
       }
-      return false;
     }
-
-    render() {
-      return (
-        <div>
-          <h2 className={styles.winnerText}>
-            {this.state.win && `Winner is ${this.state.win}`}
-          </h2>
-          <Cards
-            cards={this.state.cards}
-            handlePick={this.handlePick}
-          />
-          <button className={resetButton} onClick={() => this.resetPicks()}>Reset</button>
-        </div>
-      )
-    }
-
+    return false
   }
 
+  render () {
+    return (
+      <div>
+        <h2 className={styles.winnerText}>
+          {this.state.win && `Winner is ${this.state.win}`}
+        </h2>
+        <Cards
+          cards={this.state.cards}
+          handlePick={this.handlePick}
+        />
+        <button className={resetButton} onClick={() => this.resetPicks()}>Reset</button>
+      </div>
+    )
+  }
+}
--- stories/Cards.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-import { action } from '@storybook/addon-actions'
-
-
-export const cards = Array(9).fill('X');
-export const actions = {
-  handlePick: action('handlePick'),
-}
